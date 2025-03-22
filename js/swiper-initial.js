2cc7849e0be25f0b64d78920a9fcede0c8e5ef09
ehlwas <myramkenacc@gmail.com>
2025-03-22 20:52:30 +0400

    update

--- js/swiper-initial.js
+++ js/swiper-initial.js
@@ -3,6 +3,7 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal', // Change to 'horizontal' if needed
     // loop: true,
+    initialSlide: 1,
     autoHeight: true,
     slidesPerView: 'auto',
     centeredSlides: true,
