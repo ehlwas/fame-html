208ef33c72afd9b2e4b37924a1ed8e2a1b67b604
ehlwas <myramkenacc@gmail.com>
2025-03-24 01:08:33 +0400

    FUCK YEAH!

--- js/swiper-initial.js
+++ js/swiper-initial.js
@@ -15,3 +15,37 @@ new Swiper('#testimonialSwiper', {
         prevEl: '#testimonialPrev',
     },
 });
+
+new Swiper('#recentEventsSwiper', {
+    direction: 'horizontal', // Change to 'horizontal' if needed
+    loop: true,
+    initialSlide: 1,
+    slidesPerView: 3,
+    autoHeight: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 50,
+    allowTouchMove: true,
+    breakpoints: {
+        1200: {
+            loop: false,
+            direction: 'horizontal', // Change to 'horizontal' if needed
+            initialSlide: 1,
+            slidesPerView: 3,
+            autoHeight: true,
+            centeredSlides: true,
+            spaceBetween: 0,
+            allowTouchMove: false,
+        },
+        800: {
+            loop: false,
+            direction: 'horizontal', // Change to 'horizontal' if needed
+            initialSlide: 1,
+            slidesPerView: 2,
+            autoHeight: true,
+            centeredSlides: true,
+            spaceBetween: 0,
+            allowTouchMove: true,
+        },
+    },
+})
