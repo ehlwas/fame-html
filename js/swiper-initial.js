bb19ce6744ff9ef3dcb45d195c8809d177f5f629
ehlwas <myramkenacc@gmail.com>
2025-03-23 21:31:38 +0400

    m

--- js/swiper-initial.js
+++ js/swiper-initial.js
@@ -3,7 +3,7 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 new Swiper('#testimonialSwiper', {
     direction: 'horizontal', // Change to 'horizontal' if needed
     loop: true,
-    // initialSlide: 1,
+    initialSlide: 1,
     slidesPerView: 3,
     autoHeight: true,
     slidesPerView: 'auto',
